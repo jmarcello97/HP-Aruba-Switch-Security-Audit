69b93395722a1e84574ca2e75cd69056f3a0b45b
jasonmarcello <jkm7264@g.rit.edu>
2019-03-13 18:44:26 -0400

    added port_security_audit function that puts info into dictionary

--- library/security_audit.py
+++ library/security_audit.py
@@ -1,5 +1,25 @@
 from ansible.module_utils.basic import *
 
+f = open("text.txt", "w")
+
+def port_security_audit(module):
+	command_output = module.params['port_security']
+
+	# remove unneeded lines from show port_security command
+	command_output=command_output[5:]
+
+	port_security_config=dict()
+
+	# put port security info into a dictionary
+	# port number is the key and the value is a list of [learn mode, action, eavesdrop prevention]
+	for line in command_output:
+		line=str(line.strip())
+		info = line.split()
+		port_security_config[info[0]] = [info[1],info[3],info[4]]
+
+
+	return port_security_config
+
 def main():
 
 	fields = {
@@ -8,6 +28,8 @@ def main():
 
 	module = AnsibleModule(argument_spec=fields)
 
-	module.exit_json(changed=False, meta=module.params)
+	port_security_config = port_security_audit(module)
+
+	module.exit_json(changed=False, meta=module.params, port_security_config=port_security_config)
 
 main()
